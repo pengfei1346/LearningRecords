d6432aa84124c9527121090945e9eccd8ae21e80
cpf <chenpengfei@aicaihr.com>
2023-04-04 11:09:22 +0800

    feat:update code

--- 源码/typescript/1.js
+++ 源码/typescript/1.js
@@ -1,15 +1,71 @@
-var aa = {
-    age: 0,
-    father: "",
-    name: "",
-    hobby: [11]
-};
-function identity(arg) {
-    return arg;
-}
-console.log(identity(1));
-var a = { sex: 1 };
-var b = { name: '1111', height: 'sss' };
+// /*
+// * 如何基于一个已有类型, 扩展出一个大部分内容相似,  但是有部分区别的类型?
+// * */
+// interface Props {
+//     name: string,
+//     age: number,
+//     hobby?: number[],
+//     father: string
+// }
+// let aa: Props = {
+//     age: 0,
+//     father: "",
+//     name: "",
+//     hobby: [11]
+// }
+//
+// function identity<T>(arg: T): T {
+//     return arg;
+// }
+//
+// console.log(identity(1));
+//
+//
+// //
+//
+// interface Test {
+//     name: string;
+//     sex: number;
+//     height: string;
+// }
+//
+// type Sex = Pick<Test, 'sex'>;
+//
+// const a: Sex = { sex: 1 };
+//
+// type WithoutSex = Omit<Test, 'sex'>;
+//
+// const b: WithoutSex = { name: '1111', height: 'sss' };
+//
+//
+var Student = /** @class */ (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+        return this;
+    }
+    Student.prototype.myToString = function () {
+        return '';
+    };
+    Student.prototype.method1 = function () {
+    };
+    Student.a = 1;
+    return Student;
+}());
+// interface Person {
+//     firstName: string;
+//     lastName: string;
+// }
+//
+// function greeter(person : Person) {
+//     return "Hello, " + person.firstName + " " + person.lastName;
+// }
+//
+// let user = new Student("Jane", "M.", "User");
+//
+// greeter(user);
 // 类型注解
 /*
 * 基本类型： string number boolean undefined null symbol (bigint)
--- 源码/typescript/1.ts
+++ 源码/typescript/1.ts
@@ -1,44 +1,79 @@
 
-/*
-* 如何基于一个已有类型, 扩展出一个大部分内容相似,  但是有部分区别的类型?
-* */
-interface Props {
-    name: string,
-    age: number,
-    hobby?: number[],
-    father: string
-}
-let aa: Props = {
-    age: 0,
-    father: "",
-    name: "",
-    hobby: [11]
-}
-
-function identity<T>(arg: T): T {
-    return arg;
-}
-
-console.log(identity(1));
-
-
+// /*
+// * 如何基于一个已有类型, 扩展出一个大部分内容相似,  但是有部分区别的类型?
+// * */
+// interface Props {
+//     name: string,
+//     age: number,
+//     hobby?: number[],
+//     father: string
+// }
+// let aa: Props = {
+//     age: 0,
+//     father: "",
+//     name: "",
+//     hobby: [11]
+// }
+//
+// function identity<T>(arg: T): T {
+//     return arg;
+// }
+//
+// console.log(identity(1));
+//
+//
+// //
+//
+// interface Test {
+//     name: string;
+//     sex: number;
+//     height: string;
+// }
+//
+// type Sex = Pick<Test, 'sex'>;
+//
+// const a: Sex = { sex: 1 };
+//
+// type WithoutSex = Omit<Test, 'sex'>;
+//
+// const b: WithoutSex = { name: '1111', height: 'sss' };
+//
 //
 
-interface Test {
-    name: string;
-    sex: number;
-    height: string;
+
+class Student {
+    fullName: string;
+    static a = 1
+
+    constructor(public firstName, public middleInitial, public lastName) {
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+
+        return this
+    }
+
+    myToString() {
+        return '';
+    }
+
+    public method1 () {
+
+    }
 }
-
-type Sex = Pick<Test, 'sex'>;
-
-const a: Sex = { sex: 1 };
-
-type WithoutSex = Omit<Test, 'sex'>;
-
-const b: WithoutSex = { name: '1111', height: 'sss' };
-
-
+// @ts-ignore
+let myStudent = new Student('1--','2')
+console.log(myStudent);
+// interface Person {
+//     firstName: string;
+//     lastName: string;
+// }
+//
+// function greeter(person : Person) {
+//     return "Hello, " + person.firstName + " " + person.lastName;
+// }
+//
+// let user = new Student("Jane", "M.", "User");
+//
+// greeter(user);
 // 类型注解
 /*
 * 基本类型： string number boolean undefined null symbol (bigint)
